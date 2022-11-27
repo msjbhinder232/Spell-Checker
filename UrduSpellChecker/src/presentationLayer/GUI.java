36302779de88ae3e25aa976ce953835d1fa66640
msjbhinder232 <f200349@cfd.nu.edu.pk>
2022-11-27 17:20:21 +0500

    user interface update for use case 2

--- UrduSpellChecker/src/presentationLayer/GUI.java
+++ UrduSpellChecker/src/presentationLayer/GUI.java
@@ -1,110 +1,114 @@
 package Presentation_Layer;
 
-import java.awt.*;       // Using AWT layouts
+import java.awt.*; // Using AWT layouts
 import java.awt.event.*; // Using AWT event classes and listener interfaces
-import javax.swing.*;    // Using Swing components and containers
-
-import Business_Layer.GenerateMutants;
+import javax.swing.*; // Using Swing components and containers
 
 /**
-* Generate Mutants of Each Word Automatically:
-* -> Mutants to cross varify the actual words
-* -> Cat Mutates into Kat
-* -> Site Mutates into Sit
-*
-* @author  Muhammad Saad Javed (20F-0349)
-* @version 1.0
-* @since   2022-11-22 
-*/
+ * Generate Mutants of Each Word Automatically:
+ * -> Mutants to cross varify the actual words
+ * -> Cat Mutates into Kat
+ * -> Site Mutates into Sit
+ *
+ * @author  Muhammad Saad Javed (20F-0349)
+ * @version 1.0
+ * @since   2022-11-22 
+ */
 
 //Creating the GUI
-class MyFrame extends JFrame implements ActionListener {
-  
-  public JButton btn1, btn2;  // Declare a Button component
-  public TextField textField; // Declare a TextField component
-  public JLabel label; // Declare a Label component
-  public JTextArea textArea; // Declare a TextArea component
-  
-  public MyFrame() {
-	  
-    JPanel mainPanel = new JPanel(); // Add a JPanel into the content-pane
-    
-    setTitle("Urdu Spell Checker"); // "super" Frame sets title
-    setSize(500, 500); // "super" Frame sets initial size
-    
-    mainPanel.setLayout(null); // "super" frame sets to FlowLayout
-    mainPanel.setPreferredSize(new Dimension(500, 500)); // "super" Frame sets its initial window size
-    mainPanel.setBackground(Color.LIGHT_GRAY); // "super" Frame sets its Background color
-    
-    label = new JLabel("لفظ درج کریں"); // Add a Label into the content-pane
-    label.setBounds(180, 0, 150, 50); // "super" frame sets to Bound
-    label.setForeground(Color.white); // "super" frame sets to Foreground
-    label.setFont(new Font("Serif", Font.PLAIN, 30)); // "super" frame sets to Font
-    
-    textField = new TextField(20); // construct the TextField component with initial text
-    textField.setBounds(175, 50, 150, 30); // "super" frame sets to Bound
-    textField.setFont(new Font("Serif", Font.PLAIN, 20)); // "super" frame sets to Font
-    
-    mainPanel.add(label); // "this" Container adds the Label
-    mainPanel.add(textField); //"this" Container adds the TextField
-    
-    btn1 = new JButton("Enter");
-    btn1.setBounds(143, 115, 90, 30); // "super" frame sets to Bound
-    btn1.setBackground(Color.red); // "super" Frame sets its Background color
-    btn1.setForeground(Color.white); // "super" Frame sets its Background
-    btn2 = new JButton("Clear"); //Set b JButton
-    btn2.setBounds(253, 115, 90, 30); //Location of the b JButton
-    btn2.setBackground(Color.red); //Set Background of the JButton
-    btn2.setForeground(Color.white); //Set the font color of the JButton
-    
-    mainPanel.add(btn2); //Add b Label to panel
-    mainPanel.add(btn1); //Add b2 Label to panel
-    
-    textArea = new JTextArea(10, 30); //Set a1 JTextArea
-    textArea.setBounds(120, 150, 150, 150); //Location of the a1 JTextArea
-    textArea.setEditable(false); //Set the a1 editable to False
-    
-    mainPanel.add(textArea); //Add a1 Label to panel
-    JScrollPane scrollPane = new JScrollPane(textArea); //initializing one JScrollPane and add to a1
-    scrollPane.setBounds(45, 200, 400, 150); //Location of the scrollPane JScrollPane
-    mainPanel.add(scrollPane); //Add scrollPane Label to panel
-    add(mainPanel); //Add panel to the Frame
-    
-    btn2.addActionListener(this); //Set b to actionPerformed
-    btn1.addActionListener(this); //Set b2 to actionPerformed
-    
-    setVisible(true); //Set the frame visible to true
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set the frame with DefaultCloseOperation
+class View extends JFrame implements ActionListener {
 
+  public JTextArea inputTextArea, outputTextArea; // Declare a TextArea component
+  public JButton importDataBtn, generateMutantsBtn, highlightTyposBtn, giveSuggestionsBtn, clearBtn;
+  public JLabel inputLabel, outputLabel; // Declare a Label component
+
+  public View() {
+
+    JPanel mainPanel = new JPanel();
+
+    setTitle("Urdu Spell Checker");
+    setSize(800, 725);
+
+    mainPanel.setLayout(null);
+    mainPanel.setPreferredSize(new Dimension(500, 500));
+    mainPanel.setBackground(Color.LIGHT_GRAY);
+
+    inputLabel = new JLabel("Fetch Data");
+    inputLabel.setBounds(250, 10, 150, 50);
+    inputLabel.setForeground(Color.white);
+    inputLabel.setFont(new Font("Serif", Font.PLAIN, 30));
+    mainPanel.add(inputLabel);
+
+    inputTextArea = new JTextArea(100, 100);
+    inputTextArea.setBounds(50, 70, 525, 250);
+    inputTextArea.setEditable(false);
+    mainPanel.add(inputTextArea);
+    JScrollPane scrollPane = new JScrollPane(inputTextArea);
+    scrollPane.setBounds(50, 70, 525, 250);
+    mainPanel.add(scrollPane);
+    add(mainPanel);
+
+    outputLabel = new JLabel("Display Data");
+    outputLabel.setBounds(250, 335, 200, 50);
+    outputLabel.setForeground(Color.white);
+    outputLabel.setFont(new Font("Serif", Font.PLAIN, 30));
+    mainPanel.add(outputLabel);
+
+    outputTextArea = new JTextArea(100, 100);
+    outputTextArea.setBounds(50, 400, 525, 250);
+    outputTextArea.setEditable(false);
+    mainPanel.add(outputTextArea);
+    JScrollPane scrollPane1 = new JScrollPane(outputTextArea);
+    scrollPane1.setBounds(50, 400, 525, 250);
+    mainPanel.add(scrollPane1);
+    add(mainPanel);
+
+    importDataBtn = new JButton("Import data from XML to DB");
+    importDataBtn.setBounds(620, 250, 120, 30);
+    importDataBtn.addActionListener(this);
+
+    generateMutantsBtn = new JButton("Generate Mutants");
+    generateMutantsBtn.setBounds(620, 300, 120, 30);
+    generateMutantsBtn.addActionListener(this);
+
+    highlightTyposBtn = new JButton("Highlight Typos");
+    highlightTyposBtn.setBounds(620, 350, 120, 30);
+    highlightTyposBtn.addActionListener(this);
+
+    giveSuggestionsBtn = new JButton("Give Suggestions");
+    giveSuggestionsBtn.setBounds(620, 400, 120, 30);
+    giveSuggestionsBtn.addActionListener(this);
+
+    clearBtn = new JButton("Clear");
+    clearBtn.setBounds(620, 450, 120, 30);
+    clearBtn.addActionListener(this);
+
+    mainPanel.add(importDataBtn);
+    mainPanel.add(generateMutantsBtn);
+    mainPanel.add(highlightTyposBtn);
+    mainPanel.add(giveSuggestionsBtn);
+    mainPanel.add(clearBtn);
+
+    importDataBtn.addActionListener(this);
+    generateMutantsBtn.addActionListener(this);
+    highlightTyposBtn.addActionListener(this);
+    giveSuggestionsBtn.addActionListener(this);
+    clearBtn.addActionListener(this);
+
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
-  
+
   //Functions of the Buttons
   public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == btn2) //If button b is clicked
-    {
-      textField.setText(""); //Clear the t1 TextField
-      textArea.setText(""); //Clear the a1 JTextArea
+    if (e.getSource() == clearBtn) {
+      inputTextArea.setText("");
+      outputTextArea.setText("");
     }
-    if (e.getSource() == btn1) //If button b2 is clicked
-    {
+    if (e.getSource() == generateMutantsBtn) {}
+  }
 
-      String str = textField.getText();
-
-      // take input
-      GenerateMutants gm = new GenerateMutants(str);
-      //System.out.println(gm.implementGenerateMutants());
-
-      textArea.setFont(new Font("Serif", Font.PLAIN, 20)); //Set the a1 Font
-      textArea.setText(gm.implementGenerateMutants()); //Set the a1 output
-
-    }
+  public static void main(String[] args) {
+    new View();
   }
 }
-
-class GUI {
-	  public static void main(String[] args) {
-		  
-	    //Calling the MyFrame Class
-	    new MyFrame();
-	  }
-	}
