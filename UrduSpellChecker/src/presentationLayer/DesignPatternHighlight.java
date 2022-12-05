0e6b1e844809bd416be1f5e308e76a132983ece0
atifsiddique <f200106@cfd.nu.edu.pk>
2022-12-05 10:55:59 -0800

    Update GUI / Make table for showing SUGGESTIONS in DesignPatternHighlight.java by Atif Siddique 20F-0106

--- UrduSpellChecker/src/presentationLayer/DesignPatternHighlight.java
+++ UrduSpellChecker/src/presentationLayer/DesignPatternHighlight.java
@@ -1,37 +1,42 @@
-package lab5;
+package PresentationLayer;
 
 import java.awt.Color;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
-import java.awt.SystemColor;
 
-public class DesignPattern extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.LineBorder;
+import javax.swing.ScrollPaneConstants;
+
+public class DesignPatternHighlight extends JFrame {
 
 	private JPanel contentPane;
 	JButton Result_btn;
 	JTextArea Text1;
 	
- /*
+	 /*
      * author: Muhammad Atif Siddique
      * (20F-0106)
      * 
-     * here is GUI of highlights usecase
+     * here is code of highlights of Designing the GUI
      * */
 	JTextArea result1;
-	
-
-	public DesignPattern() {
+	private JTable table;
+	private JScrollPane scrollpane;
+	private DefaultTableModel defaultmodel;
+		
+	public DesignPatternHighlight() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 596, 353);
+		setBounds(100, 100, 596, 584);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(4, 6, 4, 6));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -43,28 +48,48 @@ public class DesignPattern extends JFrame {
 		contentPane.add(head_1);
 		
 		  Text1 = new JTextArea();
-		Text1.setBounds(51, 132, 458, 68);
+		Text1.setBounds(51, 132, 458, 30);
 		contentPane.add(Text1);
 		
 		result1 = new JTextArea();
 		result1.setBackground(new Color(176, 224, 230));
-		result1.setBounds(51, 242, 458, 31);
+		result1.setBounds(51, 191, 458, 31);
 		contentPane.add(result1);
 		
 		 Result_btn = new JButton(" کلک");
 		 Result_btn.setBackground(Color.MAGENTA);
-		Result_btn.setBounds(240, 211, 55, 23);
+		Result_btn.setBounds(235, 164, 55, 23);
 		contentPane.add(Result_btn);
 		
-		JLabel resultBtnInst = new JLabel("چیک کرنے کے لیے کلک کریں.    👈۔");
-		resultBtnInst.setFont(new Font("Serif", Font.BOLD, 10));
-		resultBtnInst.setBounds(305, 211, 144, 23);
-		contentPane.add(resultBtnInst);
+		JLabel resultBtnMemo = new JLabel("چیک کرنے کے لیے کلک کریں.    👈۔");
+		resultBtnMemo.setFont(new Font("Serif", Font.BOLD, 10));
+		resultBtnMemo.setBounds(300, 164, 144, 23);
+		contentPane.add(resultBtnMemo);
 		
 		JLabel welcome = new JLabel("(-------اردو سپیل چیکر-------)");
 		welcome.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 29));
 		welcome.setBounds(115, 22, 350, 30);
 		contentPane.add(welcome);
+		
+		table = new JTable();
+		table.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		table.setBackground(new Color(192, 192, 192));
+		table.setBounds(380, 355, 670, 328);
+		scrollpane=new JScrollPane(table,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		defaultmodel=new DefaultTableModel();
+		table.setRowSelectionAllowed(false);
+		table.setColumnSelectionAllowed(false);
+		scrollpane.setBounds(23, 268, 529, 266);
+		scrollpane.setViewportView(table);
+		contentPane.add(scrollpane);
+		table.setModel(defaultmodel);
+		this.setVisible(true);
+		defaultmodel.addColumn("suggestions");
+		defaultmodel.addColumn("Edit Distance");
+		String[] s={"Atifrow","Atif Row"};
+		defaultmodel.addRow(s);
+		
+	
 	}
 	public JButton getBtnNewButton() {
 		return Result_btn;
@@ -89,4 +114,6 @@ public class DesignPattern extends JFrame {
 	public void setTextArea_1(JTextArea textArea_1) {
 		this.result1 = textArea_1;
 	}
+	
+	
 }
